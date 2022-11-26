0b6fd7ddb5b529095b47a7b4aa2f419535123da8
Alberto Vildosola <56562400+alfalcon90@users.noreply.github.com>
2022-11-26 14:23:38 -0800

    Remove dart-define usage

--- src/+.ts
+++ src/+.ts
@@ -140,13 +140,13 @@ const completionSpec: Fig.Spec = {
           name: ["android", "a"],
           description: "Build android app",
           insertValue:
-            "\b\b\b\b\b\b\b\bflutter build appbundle --obfuscate --split-debug-info=build/app/outputs/symbols --bundle-sksl-path sksl/flutter_android.sksl.json --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_IOS_FIREBASE_APP_ID=$IOS_FIREBASE_APP_ID --dart-define=DART_ENVIRONMENT=stg --dart-define=DART_GOOGLE_REVERSED_CLIENT_ID=$GOOGLE_REVERSED_CLIENT_ID_STG --dart-define=DART_BACKGROUND_GEOLOCATION_LICENSE=$BACKGROUND_GEOLOCATION_LICENSE --dart-define=DART_BACKGROUND_GEOLOCATION_FIREBASE_LICENSE=$BACKGROUND_GEOLOCATION_FIREBASE_LICENSE --dart-define=DART_MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN --dart-define=DART_MAPBOX_STYLE_URL=$MAPBOX_STYLE_URL\n",
+            "\b\b\b\b\b\b\b\bflutter build appbundle --obfuscate --split-debug-info=build/app/outputs/symbols --bundle-sksl-path sksl/flutter_android.sksl.json \n",
         },
         {
           name: ["ios", "i"],
           description: "Build ios app",
           insertValue:
-            "\b\b\b\b\b\b\b\bflutter build ipa --obfuscate --split-debug-info=build/app/outputs/symbols --bundle-sksl-path sksl/flutter_ios.sksl.json --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_IOS_FIREBASE_APP_ID=$IOS_FIREBASE_APP_ID --dart-define=DART_ENVIRONMENT=stg --dart-define=DART_GOOGLE_REVERSED_CLIENT_ID=$GOOGLE_REVERSED_CLIENT_ID_STG --dart-define=DART_BACKGROUND_GEOLOCATION_LICENSE=$BACKGROUND_GEOLOCATION_LICENSE --dart-define=DART_BACKGROUND_GEOLOCATION_FIREBASE_LICENSE=$BACKGROUND_GEOLOCATION_FIREBASE_LICENSE --dart-define=DART_MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN --dart-define=DART_MAPBOX_STYLE_URL=$MAPBOX_STYLE_URL\n",
+            "\b\b\b\b\b\b\b\bflutter build ipa --obfuscate --split-debug-info=build/app/outputs/symbols --bundle-sksl-path sksl/flutter_ios.sksl.json\n",
         },
         {
           name: ["web", "w"],
@@ -156,13 +156,13 @@ const completionSpec: Fig.Spec = {
               name: ["prod", "p"],
               description: "Build web app for production",
               insertValue:
-                "\b\b\b\b\b\b\b\bcp web/prod.firebase-config.js web/firebase-config.js\ncp lib/generated/prod.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/prod.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_PROD --dart-define=DART_ENVIRONMENT=prod --dart-define=DART_MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN --dart-define=DART_MAPBOX_STYLE_URL=$MAPBOX_STYLE_URL\n",
+                "\b\b\b\b\b\b\b\bcp web/prod.firebase-config.js web/firebase-config.js\ncp lib/generated/prod.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/prod.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit\n",
               options: [
                 {
                   name: ["--deploy", "-d"],
                   description: "Deploy to production website",
                   insertValue:
-                    "\b\b\b\b\b\b\b\b\b\b\b\bcp web/prod.firebase-config.js web/firebase-config.js\ncp lib/generated/prod.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/prod.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_PROD --dart-define=DART_ENVIRONMENT=prod --dart-define=DART_MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN --dart-define=DART_MAPBOX_STYLE_URL=$MAPBOX_STYLE_URL\nrm -rf ../firebase/public/**\ncp -r ./build/web/. ../firebase/public\ncd ../firebase\ncp prod.firebase.json firebase.json\nfirebase deploy -P prod --only hosting:production\ncd ../app\n",
+                    "\b\b\b\b\b\b\b\b\b\b\b\bcp web/prod.firebase-config.js web/firebase-config.js\ncp lib/generated/prod.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/prod.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit\nrm -rf ../firebase/public/**\ncp -r ./build/web/. ../firebase/public\ncd ../firebase\ncp prod.firebase.json firebase.json\nfirebase deploy -P prod --only hosting:production\ncd ../app\n",
                 },
               ],
             },
@@ -170,13 +170,13 @@ const completionSpec: Fig.Spec = {
               name: ["stg", "s"],
               description: "Build web app for staging",
               insertValue:
-                "\b\b\b\b\b\b\b\bcp web/stg.firebase-config.js web/firebase-config.js\ncp lib/generated/stg.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/stg.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_ENVIRONMENT=stg --dart-define=DART_MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN --dart-define=DART_MAPBOX_STYLE_URL=$MAPBOX_STYLE_URL\n",
+                "\b\b\b\b\b\b\b\bcp web/stg.firebase-config.js web/firebase-config.js\ncp lib/generated/stg.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/stg.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit\n",
               options: [
                 {
                   name: ["--deploy", "-d"],
                   description: "Deploy to staging website",
                   insertValue:
-                    "\b\b\b\b\b\b\b\b\b\b\b\bcp web/stg.firebase-config.js web/firebase-config.js\ncp lib/generated/stg.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/stg.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_ENVIRONMENT=stg --dart-define=DART_MAPBOX_ACCESS_TOKEN=$MAPBOX_ACCESS_TOKEN --dart-define=DART_MAPBOX_STYLE_URL=$MAPBOX_STYLE_URL\nrm -rf ../firebase/public/**\ncp -r ./build/web/. ../firebase/public\ncd ../firebase\ncp stg.firebase.json firebase.json\nfirebase deploy -P stg --only hosting:staging\ncd ../app\n",
+                    "\b\b\b\b\b\b\b\b\b\b\b\bcp web/stg.firebase-config.js web/firebase-config.js\ncp lib/generated/stg.firebase_options.dart lib/generated/firebase_options.dart\ncp lib/core/domain/stg.secrets.dart lib/core/domain/secrets.dart\nflutter build web --web-renderer canvaskit\nrm -rf ../firebase/public/**\ncp -r ./build/web/. ../firebase/public\ncd ../firebase\ncp stg.firebase.json firebase.json\nfirebase deploy -P stg --only hosting:staging\ncd ../app\n",
                 },
               ],
             },
@@ -192,20 +192,18 @@ const completionSpec: Fig.Spec = {
         {
           name: ["release", "r"],
           description: "Runs flutter app in release mode",
-          insertValue:
-            "\b\b\b\b\b\b\b\bflutter run --release --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_IOS_FIREBASE_APP_ID=$IOS_FIREBASE_APP_ID\n",
+          insertValue: "\b\b\b\b\b\b\b\bflutter run --release\n",
         },
         {
           name: ["profile", "p"],
           description: "Runs flutter app in profile mode",
-          insertValue:
-            "\b\b\b\b\b\b\b\bflutter run --profile --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_IOS_FIREBASE_APP_ID=$IOS_FIREBASE_APP_ID\n",
+          insertValue: "\b\b\b\b\b\b\b\bflutter run --profile\n",
           options: [
             {
               name: ["--sksl", "-s"],
               description: "Run SkSL warmup",
               insertValue:
-                "\b\b\b\b\b\b\b\bflutter run --profile --cache-sksl --purge-persistent-cache --dart-define=DART_WEB_RECAPTCHA_SITE_KEY=$WEB_RECAPTCHA_SITE_KEY_STG --dart-define=DART_IOS_FIREBASE_APP_ID=$IOS_FIREBASE_APP_ID\n",
+                "\b\b\b\b\b\b\b\bflutter run --profile --cache-sksl --purge-persistent-cache\n",
             },
           ],
         },
